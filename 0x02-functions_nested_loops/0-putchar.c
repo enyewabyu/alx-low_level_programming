eee60be3d5ea819607c808427abd2166d9f17bfd
enyewabyu <you@example.com>
2023-02-21 08:48:54 -0800

    0-putchar print

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char *letter = "_putchar"
+
+while (*letter)
+{
+_putchar(*letter);
+letter++
+}
+_putchar('\n');
+
+return (0);
+}
