09de59c7192a824b0c2965e0e4b4838d7b3605e3
enyewabyu <enyewabyu4@gmail.com>
2023-04-07 14:58:27 -0700

    i must be chech again for the futur

--- 0x09-static_libraries/main.h
+++ 0x09-static_libraries/main.h
@@ -2,10 +2,10 @@
 #define MAIN_H
 
 /*
- * File: holberton.h
+ * File: main.h
  * Auth: Firdaus
  * Desc: Header file containing the prototypes of all functions
- *       included in the static library libholberton.a.
+ *       included in the static library main.a.
  */
 
 int _putchar(char c);
